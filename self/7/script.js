fbaea815bab354f9f438fdc89cb0d7db701b299f
Sohta0318 <nicosohta0318@gmail.com>
2021-10-03 11:31:07 -0700

    add 7

--- self/7/script.js
+++ self/7/script.js
@@ -2,4 +2,13 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function (x) {};
+var reverseNum = function (x) {
+  let reverse = Math.abs(x).toString().split("").reverse().join("");
+  if (x < 0) {
+    reverse = -Math.abs(x).toString().split("").reverse().join("");
+  }
+  return reverse;
+};
+console.log(reverseNum(123));
+console.log(reverseNum(-123));
+console.log(reverseNum(0));
