b6726ba2242fe38abd24518ce51681fefbc447d3
Sohta0318 <nicosohta0318@gmail.com>
2021-10-04 15:25:24 -0700

    add 14

--- self/14/script.js
+++ self/14/script.js
@@ -2,4 +2,21 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function (strs) {};
+var longestCommonPrefix = function (strs) {
+  if (!strs.length) return "";
+
+  for (let i = 0; i < strs[0].length; i++) {
+    // console.log(strs[0][i]);
+    for (let y = 1; y < strs.length; y++) {
+      // console.log(strs[y]);
+      // console.log(strs[0][i]);
+      // console.log(strs[y][i]);
+      if (strs[0][i] !== strs[y][i]) {
+        return strs[0].slice(0, i);
+      }
+    }
+  }
+};
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+// console.log(longestCommonPrefix([]));
